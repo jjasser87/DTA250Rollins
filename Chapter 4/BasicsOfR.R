0bfc025c5992be517e69cfaf1b88e3a3d0ff543b
JaySquare87 <jjasser@outlook.com>
2023-08-04 10:17:58 -0400

    First commit for Chapter 4

---
+++ Chapter 4/BasicsOfR.R
@@ -0,0 +1,48 @@
+# DTA250 - Fall 2023
+# Jasser Jasser
+
+# Chapter 4: Basics of R ----
+# In this chapter we will learn about R's mathematical capabilities, variables,
+# functions, and data types
+
+## Section 1: Basic Math ----
+
+# TODO
+# Type 1 + 1 below and run your code either using the 'Run' button above
+
+
+# TODO
+# Now try 3 * 7 * 2
+
+
+# TODO
+# How about 4/2
+
+
+# TODO
+# Or maybe... (4 * 6) + 5
+
+
+# TODO
+# No wait... 4 * (6 + 5)
+
+
+# TODO
+# Is the answer for (4 * 6) + 5 the same for 4 * (6 + 5). Write your answer as 
+# a comment
+
+
+## Section 2: Variables ----
+# In the R programming language, a variable is a symbol or name that stands for
+# a value and is used to store data.
+
+# TODO
+# Assign the value 2 to the variable x using the <- assignment operator
+
+
+# TODO
+# Assign the value 5 to the variable y using the <- assignment operator
+
+
+
+
