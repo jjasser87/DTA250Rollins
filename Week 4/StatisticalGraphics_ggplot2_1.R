487bcef5b3cdfdc0ef37193429a0efc62400ba58
JaySquare87 <jjasser@outlook.com>
2023-09-12 21:58:26 -0400

    ggplot2 1 finish

--- Week 4/StatisticalGraphics_ggplot2_1.R
+++ Week 4/StatisticalGraphics_ggplot2_1.R
@@ -15,8 +15,6 @@
 
 
 # Let us start by loading the ggplot2 package 
-# (ggplot always reminds me of GG Allin, the punk rocker. Don't look him up!)
-# (You don't want to be traumatized)
 
 #TODO
 # Load the ggplot2 package
@@ -32,6 +30,10 @@
 # Check the head of the diamonds dataset using the head() function
 # The head() function is a sanity check to make sure that the dataset is
 # loaded correctly
+
+
+# Diamonds (https://www.youtube.com/watch?v=lWA2pjMjpBs)
+# Carat (https://www.youtube.com/watch?v=1TsVjvEkc4s)
 
 
 ## Histogram and Densities ----
@@ -100,3 +102,89 @@
 # Inside the geom_point(), set the mapping parameter to aes()
 # Inside the aes() function, set the color parameter to the color variable
 # The color column in the diamonds dataset. It is a categorical variable.
+# factor() is a function that converts a variable to a categorical variable
+
+
+#TODO
+# Repeat the previous TODO
+# Add a + sign after the geom_point() function
+# After that use the facet_wrap() function
+# Inside the facet_wrap() function, set the formula parameter to ~ color
+
+
+#TODO
+# This time, we will replace the facet_wrap() function with the facet_grid()
+# function
+# Inside the facet_grid() function, set the formula parameter to cut ~ clarity
+
+
+# Faceting can also works with histograms or any other geom.
+
+#TODO
+# Create a histogram of the carat variable in the diamonds dataset
+# Use the ggplot() function
+# Inside the ggplot() function, set the data parameter to diamonds
+# Set the mapping parameter to aes()
+# Inside the aes() function, set the x parameter to carat
+# Add a + sign after the ggplot() function
+# After that use the geom_histogram() function
+# Add a + sign after the geom_histogram() function
+# After that use the facet_wrap() function
+# Inside the facet_wrap() function, set the formula parameter to ~ color
+
+
+## Boxplot and Violins ----
+
+### Boxplot ----
+# A boxplot is a way to show the distribution of a variable
+# It shows the median, the interquartile range, and the range of the data
+
+#TODO
+# Create a boxplot of the carat variable in the diamonds dataset
+# Use the ggplot() function
+# Inside the ggplot() function, set the data parameter to diamonds
+# Set the mapping parameter to aes()
+# Inside the aes() function, set the y parameter to carat
+# Add a + sign after the ggplot() function
+# After that use the geom_boxplot() function
+
+
+#TODO
+# Repeat the previous TODO
+# However, this time, add a an x parameter to the aes() function
+# Set the x parameter to cut
+
+
+### Violin ----
+# A violin plot is a way to show the distribution of a variable
+# It is similar to a boxplot
+# However, it also shows the density of the data at different values
+
+#TODO
+# Create a violin plot of the carat variable in the diamonds dataset
+# Use the ggplot() function
+# Inside the ggplot() function, set the data parameter to diamonds
+# Set the mapping parameter to aes()
+# Inside the aes() function, set the y parameter to carat
+# Set the x parameter to cut
+# Add a + sign after the ggplot() function
+# After that use the geom_violin() function
+
+
+## Using multiple geoms ----
+# You can use multiple geoms in the same plot
+# This is useful to show multiple aspects of the data
+
+#TODO
+# Create a violin plot of the carat variable in the diamonds dataset
+# Use the ggplot() function
+# Inside the ggplot() function, set the data parameter to diamonds
+# Set the mapping parameter to aes()
+# Inside the aes() function, set the y parameter to carat
+# Set the x parameter to cut
+# Add a + sign after the ggplot() function
+# After that use the geom_violin() function
+# Add a + sign after the geom_violin() function
+# After that use the geom_point() function
+
+
