518f4d7eb1cb2706e42b20649b97795d92575cfe
JaySquare87 <jjasser@outlook.com>
2023-09-24 19:35:48 -0400

    dplyr

---
+++ Week 5/Homework 4/Homework4.R
@@ -0,0 +1,40 @@
+# DTA250 - Fall 2023
+# Homework 4
+# I refuse to change my name. I will keep writing JJ.
+
+# Question 1 ----
+# Write a function named area_circle that calculates the area of a circle. 
+# The function should take a single argument, radius, and return the area 
+# calculated using the formula 
+# π × radius^2
+# Once the function is defined, use it to compute the area of a circle
+# with radius 5 and another with radius 10.
+# Print the results of your calculations to the console.
+
+
+# Question 2 ----
+# Write a function called even_or_odd that takes a number as an argument
+# and will print "<number> is even" is the number is even. else, it wil print
+# "<number> is odd"
+
+
+# Question 3 ----
+# Write a function named temperature_feedback that provides feedback on the 
+# weather based on the temperature value passed to it.
+# If the temperature is below 0, it should return "It's freezing cold!"
+# If the temperature is greater than 0 and less than or equal 15, it should 
+# return "It's quite chilly."
+# If the temperature is greater than 15 and less than or equal 30, it should 
+# return "The weather is pleasant."
+# If the temperature is above 30, it should return "It's scorching hot!"
+
+# hint:
+# utilize the & operator inside your if else statements
+# Example
+x <- 4
+if (x >= 0 & x <= 5){
+  print("x is less than 5 and greater than 0")
+} else {
+  print("x is greater than 5")
+}
+
