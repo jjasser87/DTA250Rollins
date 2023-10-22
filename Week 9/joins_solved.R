98b19280bb0f65b47969d5943f99159c0f13f1e9
JaySquare87 <jjasser@outlook.com>
2023-10-21 23:06:20 -0400

    joins solved

--- Week 9/joins_solved.R
+++ Week 9/joins_solved.R
@@ -9,11 +9,11 @@
 
 #TODO
 # load the tidyverse package using the library() function
-
+library(tidyverse)
 
 #TODO
 # load the nycflights13 package using the library() function
-
+library(nycflights13)
 
 ## Basic joins ----
 # There are six basic types of joins: inner, left, right, full, semi, and
@@ -27,32 +27,39 @@
 
 #TODO
 # load the flights dataset using the data() function
-
+data(flights)
 
 #TODO
 # Create a subset of flights called flights2 that contains the following columns:
 # year, time_hour, origin, dest, tailnum, and carrier.
 # use the select() function to create the subset
-
+flights2 <- flights |>
+    select(year, time_hour, origin, dest, tailnum, carrier)
 
 #TODO
 # Do a left_join() of flights2 and airlines.
-
+flights2 |>
+    left_join(airlines)
 
 #TODO
 # Do a left_join() with the weather dataset.
 # However, only keep the origin, time_hour, temp, and wind_speed columns from
 # the weather dataset.
 # Use the select() function to create the subset from the weather dataset.
-
-
+flights2 |>
+    left_join(weather |>
+                  select(origin, time_hour, temp, wind_speed)
+              )
 
 #TODO
 # Do a left_join() of flights2 and planes.
 # However, only keep the tailnum, type, engines, and seats columns from the
 # planes dataset.
 # Use the select() function to create the subset from the planes dataset.
-
+flights2 |>
+    left_join(planes |>
+                  select(tailnum, type, engines, seats)
+              )
 
 ## Specifying keys ----
 # By default, the left_join() function uses all the columns that are common
@@ -63,7 +70,8 @@
 #TODO
 # Do a left_join() of flights2 and planes.
 # use the join_by() function to specify tailnum as the key to use for the join.
-
+flights2 |>
+    left_join(planes, join_by(tailnum))
 
 # Notice the year.y column in the output.
 # This column contains the year from the planes dataset.
@@ -73,7 +81,12 @@
 # To rename the year.y column, we can use the rename() function.
 #TODO
 # Follow after me with the code
+flights2 |>
+    left_join(planes, join_by(tailnum)) |>
+    rename(trip_year = year.x, plane_manf_year = year.y)
 
+# In the above code, we renamed the year.x column to trip_year and the year.y
+# column to plane_manf_year.
 
 # join_by(tailnum) is equal to join_by(tailnum == tailnum)
 # You can join the the flights2 and airports datasets either by dest or by origin
@@ -81,11 +94,19 @@
 #TODO
 # Do a left_join() of flights2 and airports.
 # use the join_by() function and join the two datasets through dest and faa
+flights2 |>
+    left_join(airports, join_by(dest == faa))
 
+# In the above code, we joined the flights2 and airports datasets through dest
+# and faa.
 
 #TODO
 # Repeat the last TODO but join the two datasets through origin and faa
+flights2 |>
+    left_join(airports, join_by(origin == faa))
 
+# In the above code, we joined the flights2 and airports datasets through origin
+# and faa. We got a different result than the previous join.
 
 
 ## Filtering joins ----
@@ -99,13 +120,30 @@
 # in the second data frame
 
 #TODO
-# Do a semi_join() between airports and flights2.
+# Do a semi_join() between flights2 and airports.
 # use the join_by() function and join the two datasets through dest and faa
+flights2 |>
+    semi_join(airports, join_by(dest == faa))
 
+# The semi_join() function is used to return all rows from flights2 where there
+# is a match in airports based on the condition dest == faa. This means that
+# only the rows from flights2 that have a destination airport code that
+# matches an airport code in the airports data frame will be returned.
 
 #TODO
 # Do a anti_join() between flights2 and airports.
 # use the join_by() function and join the two datasets through dest and faa
 # use the distinct() function to remove duplicate dest rows
-# distinct() is a function that removes duplicate rows from a data frame.
+flights2 |>
+    anti_join(airports, join_by(dest == faa)) |>
+    distinct(dest)
 
+# The anti_join() function is used to return all rows from flights2 that do not 
+# have a match in the airports data frame based on the condition dest == faa. 
+# This means that only the rows from flights2 that do not have a destination 
+# airport code that matches an airport code in the airports data frame will be 
+# returned.
+
+# This is useful when you want to find the rows in one data frame that do not
+# have a match in another data frame.
+# In this case, we have implicit missing values in the airports data frame.
