69d3d109d63d42172ca5b40f29a08225031cb459
jjasser87 <jjasser@outlook.com>
2025-10-29 09:18:00 -0400

    String Manipulation

---
+++ Chapter 11/StringManipulation.R
@@ -0,0 +1,81 @@
+# R From Scratch: An Intro to Data Analytics
+# Chapter 11: String Manipulation
+# JJ
+# Spring 2025
+
+# Basic String Manipulation Functions ----
+
+# TODO: Create a string variable named 'text' and assign it the value 
+# "Hello, World!".
+
+# TODO: Use the nchar() function to count the number of characters in the 
+# 'text' string and print the result.
+
+# TODO: Use the substr() function to extract the substring "World" from the 
+# 'text' variable (starting from the 8th character with a length of 5 
+# characters). Print the extracted substring.
+
+# TODO: Create two string variables, 'text1' and 'text2', and assign them the 
+# values "Hello" and "World" respectively.
+
+# TODO: Use the paste() function to concatenate 'text1' and 'text2' with a 
+# space in between, and print the result.
+
+# TODO: Use the toupper() function to convert the 'text' variable 
+# ("Hello, World!") to uppercase and print the result.
+
+# TODO: Use the tolower() function to convert the 'text' variable 
+# ("Hello, World!") to lowercase and print the result.
+
+# TODO: Reassign the 'text' variable to "   Hello, World!   " 
+# (with leading and trailing spaces).
+
+# TODO: Use the trimws() function to remove leading and trailing whitespace 
+# from the 'text' variable and print the trimmed string.
+
+# Advanced String Manipulation Functions ----
+
+# TODO: Load the stringr package using library().
+# Or you can load the entire tidyverse library if you prefer.
+
+# TODO: Reset the 'text' variable to its original value: "Hello, World!".
+
+# TODO: Use the str_length() function from the stringr package to count the 
+# number of characters in the 'text' string and print the result.
+
+# TODO: Use the str_sub() function from the stringr package to extract the 
+# substring "World" from the 'text' variable (starting from the 8th 
+# character, ending at the 12th). Print the extracted substring.
+
+# TODO: Use the str_c() function from the stringr package to concatenate 
+# 'text1' ("Hello") and 'text2' ("World") without any separator, and print 
+# the result.
+
+# TODO: Use the str_c() function to concatenate 'text1' and 'text2' with a 
+# space as the separator (sep = " "), and print the result.
+
+# TODO: Use the str_to_upper() function to convert the 'text' variable 
+# ("Hello, World!") to uppercase and print the result.
+
+# TODO: Use the str_to_lower() function to convert the 'text' variable 
+# ("Hello, World!") to lowercase and print the result.
+
+# TODO: Reassign the 'text' variable to "   Hello, World!   " 
+# (with leading and trailing spaces).
+
+# TODO: Use the str_trim() function to remove leading and trailing whitespace 
+# from the 'text' variable and print the trimmed string.
+
+# TODO: Reset the 'text' variable to "Hello, World!".
+
+# TODO: Use the str_replace() function to replace the substring "World" 
+# in 'text' with "Universe" and print the modified string.
+
+# TODO: Use the str_detect() function to check if the 'text' variable contains 
+# the substring "World" and print the result (TRUE or FALSE).
+
+# TODO: Use the str_count() function to count the number of occurrences of the 
+# letter "o" in the 'text' variable and print the count.
+
+# TODO: Use the str_extract_all() function to extract all occurrences of the 
+# letter "o" from the 'text' variable and print the result (as a list).
